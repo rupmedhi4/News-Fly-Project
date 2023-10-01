14d4ce409b45f6799bdb686506adaf2990553efc
Rup medhi <rupmmedhi@gmail.com>
2023-10-01 13:04:46 +0530

    env

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,8 +1,7 @@
 const Config = {
-    // api_key: process.env.REACT_APP_API_KEY,
-    // base_url: process.env.REACT_APP_BASE_KEY
-    api_key: ee6a9310198546eaa131e86129df8f74,
-     base_url: https://newsapi.org/v2/
+    api_key: process.env.REACT_APP_API_KEY,
+    base_url: process.env.REACT_APP_BASE_KEY
+  
   };
   
   export default Config;
